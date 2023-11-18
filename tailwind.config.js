b13f0ccec568bcedfcf68a46937a51812cabe1e3
Gabor Guban <gab95or@gmail.com>
2023-11-18 16:27:27 +0100

    Basic colors added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [],
+    theme: {
+        extend: {},
+    },
+    colors: {
+        backgroundColor: '#201F41',
+        primaryColor: '#D1788F',
+        sedcondaryColor: '#422D53',
+        accentColor: '#E4ACB2',
+        textColor: '#FFFFFF',
+    },
+    plugins: [],
+};
