d35cb904b3e38a6dd585132444a538ca76649827
Gabor Guban <gab95or@gmail.com>
2023-12-02 19:00:34 +0100

    Sanity time props solved

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,8 +1,10 @@
 export async function getStaticPaths() {
   const posts = await useSanityClient().fetch('*[_type == "funtazia"]');
 
-  return posts.map(({ slug, content, headline, image }) => ({
-    params: { slug: slug.current },
-    props: { content, headline, image },
-  }));
+  return posts.map(
+    ({ slug, content, headline, image, author, publishedAt }) => ({
+      params: { slug: slug.current },
+      props: { content, headline, image, author, publishedAt },
+    })
+  );
 }
