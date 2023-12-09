7d65effd1efbc181d1c1fc59357d07cd8a6374aa
Gabor Guban <gab95or@gmail.com>
2023-12-09 21:34:25 +0100

    Adding gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwindIntegration from '@astrojs/tailwind';
 import sanity from 'astro-sanity';
 
 export default defineConfig({
-  site: 'https://adventure-timeél.github.io/',
+  site: 'https://adventure-time.github.io/',
+  base: '/adventure-time',
   integrations: [
     sanity({
       projectId: 'ypaaup99',
