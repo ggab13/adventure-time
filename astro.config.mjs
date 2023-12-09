808822fb49de61375fdc81dcfeb0f2476ae4cb75
Gabor Guban <gab95or@gmail.com>
2023-12-09 21:52:22 +0100

    Base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sanity from 'astro-sanity';
 
 export default defineConfig({
   site: 'https://ggab13.github.io/',
-
+  base: '',
   integrations: [
     sanity({
       projectId: 'ypaaup99',
